6082b396dd3845ed030877731f513300c066b7ff
Ed Michaelson <edwardmichaelson@gmail.com>
2022-01-10 16:33:35 +0800

    add post endpoint to botAPI

--- botAPI.py
+++ botAPI.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+import pprint
 
 botApp = Flask(__name__)
 
-@botApp.route('/')
-def testFunc():
-	try:
-		print('hello sophie!')
-	except:
-		pass	
-	return 'hello sophie!'
+@botApp.get('/')
+def placeholderText():
+	return 'This is a placeholder page for Ed\'s Groupme chat boi API'
+
+@botApp.post('/')
+def recMessage():
+	print('botAPI hit by Groupme, message received')
+	if request.is_json:
+		message = request.get_json()
+		pprint.pprint(message)
+		return 200
+	return {'error': 'Request must be JSON'}, 415
 
 if __name__ == '__main__':
 	botApp.run()
