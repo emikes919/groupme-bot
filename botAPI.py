40d140b5ddce0ce2e0bc0e1047d2aabecb10ddea
Ed Michaelson <edwardmichaelson@gmail.com>
2022-01-10 16:19:59 +0800

    update testfunc w/return try/except

--- botAPI.py
+++ botAPI.py
@@ -4,7 +4,11 @@ botApp = Flask(__name__)
 
 @botApp.route('/')
 def testFunc():
-	print('hello sophie!')
+	try:
+		print('hello sophie!')
+	except:
+		pass	
+	return 'hello sophie!'
 
 if __name__ == '__main__':
 	botApp.run()
