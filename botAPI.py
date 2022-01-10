157ac3c007aeac72fb0cb8379a7140e242379bd0
Ed Michaelson <edwardmichaelson@gmail.com>
2022-01-10 16:40:30 +0800

    updated return response status 200

--- botAPI.py
+++ botAPI.py
@@ -5,7 +5,7 @@ botApp = Flask(__name__)
 
 @botApp.get('/')
 def placeholderText():
-	return 'This is a placeholder page for Ed\'s Groupme chat boi API'
+	return 'This is a placeholder page for Ed\'s Groupme chat bot API'
 
 @botApp.post('/')
 def recMessage():
@@ -13,7 +13,7 @@ def recMessage():
 	if request.is_json:
 		message = request.get_json()
 		pprint.pprint(message)
-		return 200
+		return 'Success!', 200
 	return {'error': 'Request must be JSON'}, 415
 
 if __name__ == '__main__':
