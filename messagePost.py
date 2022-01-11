ab153b693de71d6eeddc030d38d1e01fb73cc1ec
Ed Michaelson <edwardmichaelson@gmail.com>
2022-01-11 20:44:19 +0800

    updated message selection logic to non random

--- messagePost.py
+++ messagePost.py
@@ -7,8 +7,11 @@ tokenParam = '?token=' + config.TOKEN
 botID = config.botID
 messageList = config.messageList
 
+num = 0
+
 def sendMessage():
-	num = random.randint(0, len(messageList) - 1)
+	global num
+	#num = random.randint(0, len(messageList) - 1)
 	text = messageList[num]
 	url = BASEURL + endpoint + tokenParam
 	message = {
@@ -17,4 +20,9 @@ def sendMessage():
 	}
 	response = requests.post(url, json=message)
 	print(response.status_code)
+
+	num += 1
+	if num == len(messageList):
+		num == 0
+
 	return response
