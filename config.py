8549c5e0dc8f2461b514faa4731d3690d4cc50ea
Ed Michaelson <91359603+emikes919@users.noreply.github.com>
2022-02-24 17:38:17 +0800

    Removed old messages pre public repo

--- config.py
+++ config.py
@@ -47,20 +47,3 @@ botID = '9c1a8bbdb76f9d652dcbf2d57e'
 messageList = [
 	''
 ]
-
-oldMessageList = [
-	'dammit Will! remember what we talked about! Think before I type. Think before I type. Think before I type.',
-	'what I really meant to say was: Hi I\'m Will, I\'m a big dumb idiot',
-	'FUCK. i shouldn\'t have said that, i\'m so insecure! At least I\'m not as short as Ed, Steve or, heaven forbid, Sam',
-	'blah blah blah, i\'m a silly moron',
-	'I wish I could say what I\'m actually thinking. i love big dongs in my face',
-	'i just want to take a second to let everyone know, I have a huge penis. Just kidding! It\'s tiny!',
-	'I wonder if anyone knows I want to blow mike',
-	'why can\'t I get "joel is hot" out of my dang head!',
-	'I hope the fellas don\'t realize I\'m an easy target',
-	'Do me Wes. Wait did I say that out loud?',
-	'Very good Will!',
-	'The Giants suck!',
-	'I am the smartest man on earth',
-	'Dont\'t worry bud, if the guys are too hard on us, mommy will cuddle us'
-]
